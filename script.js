2f0c899ff9c4b5745c5ec9eb6ed5ab825130daee
Guimg4689 <guilherme.martins20@hotmail.com>
2023-09-20 18:28:51 -0300

    trabalho 5 de ppi

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function (event) {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
 
-    if (email === '') {
-        document.getElementById('email').style.border = '1px solid red';
-        document.getElementById('emailError').textContent = 'O campo E-mail deve ser preenchido.';
-        event.preventDefault();
-    } else {
-        document.getElementById('email').style.border = '1px solid #ccc';
-        document.getElementById('emailError').textContent = '';
-    }
+        if (email === '') {
+            document.getElementById('email').classList.add('is-invalid');
+            document.getElementById('emailError').textContent = 'O campo E-mail deve ser preenchido.';
+            event.preventDefault();
+        } else {
+            document.getElementById('email').classList.remove('is-invalid');
+            document.getElementById('emailError').textContent = '';
+        }
 
-    if (password === '') {
-        document.getElementById('password').style.border = '1px solid red';
-        document.getElementById('passwordError').textContent = 'O campo Senha deve ser preenchido.';
-        event.preventDefault();
-    } else {
-        document.getElementById('password').style.border = '1px solid #ccc';
-        document.getElementById('passwordError').textContent = '';
-    }
+        if (password === '') {
+            document.getElementById('password').classList.add('is-invalid');
+            document.getElementById('passwordError').textContent = 'O campo Senha deve ser preenchido.';
+            event.preventDefault();
+        } else {
+            document.getElementById('password').classList.remove('is-invalid');
+            document.getElementById('passwordError').textContent = '';
+        }
+    });
 });
